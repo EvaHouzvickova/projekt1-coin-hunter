c10b7969e6533cbf9c15e2928e89603c08c1c41d
Eva Houžvičková <houzvickova@icloud.com>
2022-02-26 15:07:13 +0100

    function for moving figure ready

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ let figurka = document.getElementById('panacek');
 let startovniPozice = 
 figurka.style.top = "50%";
 figurka.style.left = "50%";
+
+let pohyb = 3;
+
+function pohybPanacka(event) {
+	if (event.keyCode === 40) { //keydown
+		figurka.style.top = parseInt(figurka.style.top) + pohyb + "%";
+	} else if (event.keyCode === 38) { //keyup
+		figurka.style.top = parseInt(figurka.style.top) - pohyb + "%";
+	} else if (event.keyCode === 37) { //keyleft
+		figurka.style.left = parseInt(figurka.style.left) - pohyb + "%";
+	} else { //keyright
+		figurka.style.left = parseInt(figurka.style.left) + pohyb + "%";
+	}
+}
