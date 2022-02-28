64a797ee4545d8f628a5edc47d9bcfc56166189b
Eva Houžvičková <houzvickova@icloud.com>
2022-02-28 14:28:17 +0100

    Function onload created with generating the random position of the coin

--- main.js
+++ main.js
@@ -8,24 +8,35 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 // sem začni psát svůj program
 
-let figurka = document.getElementById('panacek');
+let panacek = document.getElementById('panacek');
 let startovniPozice = 
-figurka.style.top = parseInt(window.innerHeight/2) + "px";
-figurka.style.left = parseInt(window.innerWidth/2) + "px";;
+panacek.style.top = parseInt(window.innerHeight/2) + "px";
+panacek.style.left = parseInt(window.innerWidth/2) + "px";;
 
 let pohyb = 20;
 
 function pohybPanacka(event) {
-	let figurkaY = figurka.style.top;
-	let figurkaX = figurka.style.left;
+	let panacekY = panacek.style.top;
+	let panacekX = panacek.style.left;
 
-	if (event.keyCode === 40 && (parseInt(figurkaY) + pohyb) <= window.innerHeight) { //keydown
-		figurka.style.top = parseInt(figurkaY) + pohyb + "px";
-	} if (event.keyCode === 38 && (parseInt(figurkaY) - pohyb) >= 0) { //keyup
-		figurka.style.top = parseInt(figurkaY) - pohyb + "px";
-	} if (event.keyCode === 37 && (parseInt(figurkaX) + pohyb) >= 0) { //keyleft
-		figurka.style.left = parseInt(figurkaX) - pohyb + "px";
-	} if (event.keyCode === 39 && (parseInt(figurkaX) - pohyb) <= window.innerWidth) { //keyright
-		figurka.style.left = parseInt(figurkaX) + pohyb + "px";
+	if (event.keyCode === 40 && (parseInt(panacekY) + pohyb) <= window.innerHeight) { //keydown
+		panacek.style.top = parseInt(panacekY) + pohyb + "px";
+	} if (event.keyCode === 38 && (parseInt(panacekY) - pohyb) >= 0) { //keyup
+		panacek.style.top = parseInt(panacekY) - pohyb + "px";
+	} if (event.keyCode === 37 && (parseInt(panacekX) + pohyb) >= 0) { //keyleft
+		panacek.style.left = parseInt(panacekX) - pohyb + "px";
+	} if (event.keyCode === 39 && (parseInt(panacekX) - pohyb) <= window.innerWidth) { //keyright
+		panacek.style.left = parseInt(panacekX) + pohyb + "px";
 	}
+}
+
+let minceY = nahodnaPoziceMince(0,window.innerHeight)
+let minceX = nahodnaPoziceMince(0,window.innerWidth)
+let mince = document.getElementById('mince');
+
+mince.style.top = minceY + "px";
+mince.style.left = minceX + "px";
+
+function nahodnaPoziceMince(min, max) {
+	return Math.floor(Math.random()*(max - min + 1) + min);
 }
