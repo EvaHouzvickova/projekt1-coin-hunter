8bfa650211e93eeaade0482a5aa395f30cc8e486
Eva Houžvičková <houzvickova@icloud.com>
2022-03-03 21:29:23 +0100

    music is playing + trying to fix the problem with moving the coin after being covered by figure - NOT FIXED YET

--- main.js
+++ main.js
@@ -5,30 +5,39 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 }
 */
 
-
 // sem začni psát svůj program
 
 let panacek = document.getElementById('panacek');
-let panacekY = parseInt(window.innerHeight/2);
-let panacekX = parseInt(window.innerWidth/2);
+let panacekY = window.innerHeight / 2;
+let panacekX = window.innerWidth / 2;
+panacek.style.top = panacekY + "px";//vychozí pozice panáčka
+panacek.style.left = panacekX + "px";
 let panacekSirka = 64;
 let panacekVyska = 70;
 let pohyb = 20;
 
 let mince = document.getElementById('mince');
-minceY = Math.floor(Math.random()* window.innerHeight); 
-minceX = Math.floor(Math.random()*window.innerWidth );
 let minceSirka = 36;
 let minceVyska = 36;
 
+let hudba = document.getElementById('hudba');
+hudba.play();
 
-function priNacteniStranky() {
-	panacek.style.top = panacekY + "px";//vychozí pozice panáčka
-	panacek.style.left = panacekX + "px";
+function generatorMince() {
+	minceY = Math.floor(Math.random() * window.innerHeight);
+	minceX = Math.floor(Math.random() * window.innerWidth);
 	mince.style.top = minceY + "px";
 	mince.style.left = minceX + "px";
 }
 
+function panacekSebralMinci() {
+	if (!(panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
+		minceY = Math.floor(Math.random() * window.innerHeight);
+		minceX = Math.floor(Math.random() * window.innerWidth);
+		mince.style.top = minceY + "px";
+		mince.style.left = minceX + "px";	
+	}
+}
 
 function pohybPanacka(event) {
 	let panacekY = panacek.style.top;
@@ -44,17 +53,7 @@ function pohybPanacka(event) {
 		panacek.src = "obrazky/panacek-vlevo.png";
 		panacek.style.left = parseInt(panacekX) - pohyb + "px";
 	} if (event.keyCode === 39 && (parseInt(panacekX) - pohyb) <= window.innerWidth) {//keyright
-		panacek.src = "obrazky/panacek-vpravo.png"; 
+		panacek.src = "obrazky/panacek-vpravo.png";
 		panacek.style.left = parseInt(panacekX) + pohyb + "px";
-	}
-}
-
-function panacekSebralMinci() {
-
-	if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
-		minceY = Math.floor(Math.random()* window.innerHeight);
-		minceX = Math.floor(Math.random()*window.innerWidth );
-		mince.style.top = minceY + "px";
-		mince.style.left = minceX + "px";
 	}
 }
