876c7bc10c12d310f1f0bb15582b57704c4b7975
Eva Houžvičková <houzvickova@icloud.com>
2022-03-03 23:32:49 +0100

    Fixed problem with generating new position of coin after beeing "eaten" by figure

--- main.js
+++ main.js
@@ -35,6 +35,11 @@ function generatorPoziceMince() {
 	mince.style.left = minceX + "px";
 }
 
+function panacekSebralMinci() { 
+		generatorPoziceMince();
+		zvukMince.play();	
+	}
+
 function pohybPanacka(event) {
 	let panacekY = panacek.style.top;
 	let panacekX = panacek.style.left;
@@ -42,6 +47,7 @@ function pohybPanacka(event) {
 	if (event.keyCode === 40 && (parseInt(panacekY) + pohyb) <= window.innerHeight) { //keydown
 		panacek.src = "obrazky/panacek.png";
 		panacek.style.top = parseInt(panacekY) + pohyb + "px";
+	
 	} if (event.keyCode === 38 && (parseInt(panacekY) - pohyb) >= 0) { //keyup
 		panacek.src = "obrazky/panacek-nahoru.png";
 		panacek.style.top = parseInt(panacekY) - pohyb + "px";
@@ -51,12 +57,7 @@ function pohybPanacka(event) {
 	} if (event.keyCode === 39 && (parseInt(panacekX) - pohyb) <= window.innerWidth) {//keyright
 		panacek.src = "obrazky/panacek-vpravo.png";
 		panacek.style.left = parseInt(panacekX) + pohyb + "px";
-	}
-}
-
-function panacekSebralMinci() {
-	if (!(panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
-		generatorPoziceMince();
-		zvukMince.play();	
+	} if (!(parseInt(panacekX) + panacekSirka < minceX || minceX + minceSirka < parseInt(panacekX) || parseInt(panacekY) + panacekVyska < minceY || minceY + minceVyska < parseInt(panacekY))) {
+		panacekSebralMinci();
 	}
 }
